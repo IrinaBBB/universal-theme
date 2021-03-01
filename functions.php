a1a5a5d571014280e02332eec806de254d708369
IrinaBBB <balkarovairina@gmail.com>
2021-03-01 22:57:01 +0100

    created: header on main page

--- functions.php
+++ functions.php
@@ -1,6 +1,31 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+
+// Добавление дополнительных возможностей
+if (! function_exists('universal_theme_setup')) : 
+    function universal_theme_setup() {
+        // Добавление тега title
+        add_theme_support( 'title-tag' ); 
+
+        // Добавление пользовательского логотипа
+        add_theme_support( 'custom-logo', [
+            'width'                  => 163,
+            'flex-height'            => true,
+            'header-text'            => 'Universal',
+            'unlink-homepage-logo'   => false
+        ]);
+
+        // Регистрация меню
+            register_nav_menus([
+                'header_menu' => 'Меню в шапке',
+                'footer_menu' => 'Меню в подвале'
+            ]);
+    }
+endif;
+add_action( 'after_setup_theme', 'universal_theme_setup' );
+
+// Подключение стилей и шрифтов
 function enqueue_universal_style() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style('universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style', null, null);
+    wp_enqueue_style('universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style');
 }
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
