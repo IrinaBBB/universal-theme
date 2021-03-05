d6c5a61444dcc2561cbcad3b61d5e89bd47a62c1
IrinaBBB <balkarovairina@gmail.com>
2021-03-05 09:34:07 +0100

    add: showing acticles on main page

--- front-page.php
+++ front-page.php
@@ -4,43 +4,76 @@
     <div class="container">
         <div class="hero">
             <div class="left">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/image.jpg' ?>" alt="" class="post-thumb">  
-                <a href="#" class="author">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="Avatar: author" class="avatar">
-                    <div class="author-bio">
-                        <span class="author-name">Имя Автора</span>
-                        <span class="author-rank">Должность</span>
-                    </div>
-                </a>
-            <div class="post-text">
-                <a href="#" class="category-name">Рубрики</a>
-                <h2 class="post-title">Название поста</h2>
-                <a href="#" class="more">Читать далее</a>
+                <?php
+                    global $post;
+
+                    $myposts = get_posts([
+                        'numberposts' => 1,
+                        'category_name' => 'JavaScript'
+                    ]);
+
+                    // Проверяем, есть ли посты
+                    if ($myposts) {
+                        // Если есть, запускаем цикл
+                        foreach ($myposts as $post) {
+                            setup_postdata( $post );
+                ?>
+                    <img src="<?php the_post_thumbnail_url(); ?> " alt="" class="post-thumb">
+                            <?php $author_id = get_the_author_meta('ID'); ?>
+                        <a href="<?php echo get_author_posts_url($author_id); ?>" class="author">
+                            <img src="<?php echo get_avatar_url($author_id); ?>" alt="Avatar: author" class="avatar">
+                            <div class="author-bio">
+                                <span class="author-name"><?php the_author(); ?></span>
+                                <span class="author-rank">Должность</span>
+                            </div>
+                        </a>
+                    <div class="post-text">
+                <?php the_category(); ?>
+                <h2 class="post-title"><?php the_title(); ?></h2>
+                <a href="<?php echo get_the_permalink(); ?>" class="more">Читать далее</a>
             </div>
+            <?php  
+                }
+                } else {
+                ?><p>Постов нет</p><?php 
+            }
+
+            wp_reset_postdata();
+            ?>
             </div>
             <div class="right">
                 <h3 class="recommend">Рекомендуем</h3>
+
                 <ul class="posts-list">
-                    <li class="post">
-                        <span class="category-name">Категория</span>
-                        <h4 class="post-title">Название поста <br>в две строки</h4>
-                    </li>
-                    <li class="post">
-                        <span class="category-name">Категория</span>
-                        <h4 class="post-title">Название поста <br>в две строки</h4>
-                    </li>
-                    <li class="post">
-                        <span class="category-name">Категория</span>
-                        <h4 class="post-title">Название поста <br>в две строки</h4>
-                    </li>
-                    <li class="post">
-                        <span class="category-name">Категория</span>
-                        <h4 class="post-title">Название поста <br>в две строки</h4>
-                    </li>
-                    <li class="post">
-                        <span class="category-name">Категория</span>
-                        <h4 class="post-title">Название поста <br>в две строки</h4>
-                    </li>
+                 <?php
+            
+                    // Объявляем глобальную переменную $post
+                    global $post;
+
+                    $myposts = get_posts([
+                        'numberposts' => 5,
+                        'offset' => 1
+                    ]);
+                    // Проверяем, есть ли посты
+                    if ($myposts) {
+                        // Если есть, запускаем цикл 
+                foreach ($myposts as $post) {
+                    setup_postdata( $post );
+                    
+                ?>
+                        <li class="post">
+                                <?php the_category(); ?>
+                                <a class="post-permalink" href="<?php echo get_the_permalink(); ?>"><h4 class="post-title"><?php the_title(); ?></h4></a>
+                        </li>
+            
+                    <?php  
+                                }
+                    } else {
+                        ?><p>Постов нет</p><?php        
+                    }
+
+                wp_reset_postdata();
+            ?>      
                 </ul>
             </div>
         </div>
--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ if (! function_exists('universal_theme_setup')) :
         // Добавление тега title
         add_theme_support( 'title-tag' ); 
 
+        // Добавление миниатюр
+        add_theme_support( 'post-thumbnails', array('post') );
+
         // Добавление пользовательского логотипа
         add_theme_support( 'custom-logo', [
             'width'                  => 163,
@@ -27,7 +30,7 @@ add_action( 'after_setup_theme', 'universal_theme_setup' );
 function enqueue_universal_style() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_style('universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style');
-    wp_enqueue_style('Roboto-Slab', '<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet">');
+//    wp_enqueue_style('Roboto-Slab', '<link rel="preconnect" href="https://fonts.gstatic.com">
+//<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet">');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
